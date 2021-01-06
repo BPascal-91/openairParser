f7f23dcd5cc9e36da287499f4848ace4fe9d6229
BPascal-91 <pascal_bazile@yahoo.fr>
2021-01-07 00:24:15 +0100

    v1.2.6

--- src/bpaTools/__init__.py
+++
@@ -1,9 +0,0 @@
-from .Logger import Logger
-from .ProgressBar import ProgressBar
-from .GeoCoordinates import geoStr2dd, geoDd2dms, geoDd2dmd
-from .Tools import theQuit, sysExitError, ctrlPythonVersion, initEvent, getFileName, getFilePath, getNow, getNowISO, getDateNow, getDate, getVersionFile, readJsonFile, writeJsonFile, writeTextFile, defaultEncoding, encodingUTF8, createFolder, deleteFile, getCommandLineOptions
-
-__all__ = ([Logger] +
-           [ProgressBar] +
-		   [geoStr2dd, geoDd2dms, geoDd2dmd] +
-           [theQuit, sysExitError, ctrlPythonVersion, initEvent, getFileName, getFilePath, getNow, getNowISO, getDateNow, getDate, getVersionFile, readJsonFile, writeJsonFile, writeTextFile, defaultEncoding, encodingUTF8, createFolder, deleteFile, getCommandLineOptions])
